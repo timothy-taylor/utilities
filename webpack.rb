caa5edd80689e924c03ecdb9e41f87a7e8e46eb2
Tim <timothy.george.taylor@gmail.com>
2021-12-22 12:28:21 -0700

    updated webpack setup to load jest and babel

--- webpack.rb
+++ webpack.rb
@@ -1,15 +1,9 @@
 # frozen_string_literal: true
 
-# creating a new instance of this class
-# will create a basic skeleton for webpack
-# with css, hmtl, image
-# uses system calls (touch, npm, git),
-# ruby methods,
-# and vim for file editing
-class SetupWebpack
+module Constants
   INDEX_PATH = 'src/index.js'
-  INDEX =
-    "import './style.css';
+  INDEX = <<-HTML
+    import './style.css';
 
     function component() {
       const element = document.createElement('div');
@@ -17,10 +11,12 @@ class SetupWebpack
       return element;
     }
 
-    document.body.appendChild(component());"
+    document.body.appendChild(component());
+  HTML
+
   WEBPACKCONFIG_PATH = 'webpack.config.js'
-  WEBPACKCONFIG =
-    "const path = require('path');
+  WEBPACKCONFIG = <<-WEBPACK
+    const path = require('path');
     const HtmlWebpackPlugin = require('html-webpack-plugin');
     const Dotenv = require('dotenv-webpack');
 
@@ -47,9 +43,58 @@ class SetupWebpack
             test: /\.(png|svg|jpg|jpeg|gif)$/i,
             type: 'asset/resource',
           },
+          {
+            test: /\.m?js$/,
+            exclude: /node_modules/,
+            use: {
+              loader: 'babel-loader',
+              options: {
+                presets: [
+                  ['@babel/preset-env', { targets: 'defaults' }]
+                ]
+              }
+            }
+          }
         ],
       },
-    };"
+    };
+  WEBPACK
+
+  BABELRC = <<-BABEL
+    {
+      "presets": [["env", {"modules": false}]],
+
+      "plugins": ["syntax-dynamic-import"],
+
+      "env": {
+        "test": {
+          "plugins": ["dynamic-import-node"]
+        }
+      }
+    }
+  BABEL
+
+  JESTPACKAGE = <<-JSON
+    "jest": {
+      "moduleFileExtensions": ["js", "jsx"],
+      "moduleDirectories": ["node_modules", "bower_components", "shared"],
+      "moduleNameMapper": {
+        "\\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": "<rootDir>/__mocks__/fileMock.js",
+        "\\\.(css|less)$": "identity-obj-proxy"
+      }
+    }
+  JSON
+end
+
+# creating a new instance of this class
+# will create a basic skeleton for webpack
+# with css, hmtl, image
+# babel, jest
+# uses system calls (touch, npm, git),
+# ruby methods,
+# and vim for file editing
+class SetupWebpack
+  include Constants
 
   def initialize
     basic_start
@@ -57,7 +102,9 @@ class SetupWebpack
     git_init
     add_dotenv
     create_config_file
+    system('npm install -D babel-loader @babel/core @babel/preset-env webpack')
     add_npm_build_watch
+    add_jest
   end
 
   def basic_start
@@ -103,6 +150,19 @@ class SetupWebpack
   def add_npm_build_watch
     system('vim -c "7 s/$/,\r\t\"watch\": \"webpack --watch\"/" -c "wq" package.json')
     system('vim -c "8 s/$/,\r\t\"build\": \"webpack\"/" -c "wq" package.json')
+  end
+
+  def add_jest
+    system('npm install --save-dev jest')
+    system('npm install --save-dev identity-obj-proxy')
+    Dir.mkdir('__mocks__')
+    system('touch __mocks__/fileMock.js')
+    filemock = "module.exports = 'test-file-stub';"
+    File.open('__mocks__/fileMock.js', 'w') { |file| file.write(filemock) }
+    system('touch .babelrc')
+    File.open('.babelrc', 'w') { |file| file.write(BABELRC) }
+    puts 'please update the test command to "jest" and add the following to your package.json:'
+    puts JESTPACKAGE
   end
 end
 
